df6d82272681ccc43f1a88260e5b674c042c7243
guozongkui <guozongkui@rongcloud.cn>
2020-08-30 17:31:34 +0800

    finish quick-import demo

---
+++ app/src/main/java/com/example/rongcloud_android_imdemo_import/ConversationActivity.java
@@ -0,0 +1,25 @@
+package com.example.rongcloud_android_imdemo_import;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import io.rong.imkit.fragment.ConversationFragment;
+
+public class ConversationActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+        ConversationFragment conversationFragment=new ConversationFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, conversationFragment);
+        transaction.commit();
+
+    }
+}
